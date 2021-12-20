ce37d1161be47b9b9d5ef4457169745b61686e69
corentinmaillard <33122169+corentinmaillard@users.noreply.github.com>
2021-12-20 22:34:37 +0100

    Update server.js

--- server.js
+++ server.js
@@ -8,12 +8,6 @@ let rooter = require("./routes");
 
 let mysql = require('mysql');
 
-let tricks = false;
-
-let idCheck = [0,0,0] //initialisation de ma variable (utiliser pour verfier qu'il n'y aient pas 2fois la même formation)
-
-let listCatalogue = []; //créer une liste vide
-
 let cookieParser = require('cookie-parser')//initialise le midlleware
 
 let session = require('express-session'); //initialise la session et utilise le midlleware express-session
